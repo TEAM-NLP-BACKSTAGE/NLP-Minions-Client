1e3226e43bfbd1e91fcef149a3597dc544df61bc
nohgyumyung <gyumyung1995@gmail.com>
2020-10-03 20:16:55 +0900

    Loading 레이아웃 변경 및 로딩 이미지 추가

--- Loading.js
+++ Loading.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { StyleSheet, Image, ImageBackground, StatusBar } from 'react-native';
+import { StyleSheet, Image, ImageBackground, StatusBar, View, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window')
 
 export default function Loading() {
     return(
         <ImageBackground source={require('./assets/loading/배경.jpg')} style={styles.container}>
             <StatusBar barStyle='light-content'></StatusBar>
             <Image style={styles.logo} source={require('./assets/loading/웨일링.png')}/>
+            <View style={styles.loadingIndicators}>
+                <Image style={{width: 19, height: 19}} source={require('./assets/loading/로딩1.png')}/>
+                <Image style={{width: 19, height: 19, marginHorizontal: 7}} source={require('./assets/loading/로딩2.png')}/>
+                <Image style={{width: 19, height: 19}} source={require('./assets/loading/로딩3.png')}/>
+                <Image style={{width: 19, height: 19, marginHorizontal: 7}} source={require('./assets/loading/로딩4.png')}/>
+                <Image style={{width: 25, height: 19}} source={require('./assets/loading/로딩5.png')}/>
+            </View>
         </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center'
+        flex: 1,
+        alignItems: 'center'
       },
     logo : {
         width: 186,
-        height: 189
+        height: 189,
+        marginTop: height / 2 - 90
+    },
+    loadingIndicators: {
+        flexDirection: 'row',
+        marginTop: height / 8
     }
   });
